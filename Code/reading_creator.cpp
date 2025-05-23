b64ff1431bc5c253ed07d06e97cbfef77f23068b
Roko Barišić <roko.barisic@fer.unizg.hr>
2025-05-24 01:40:47 +0200

    Added reading_creator.cpp
    Added reading_creator.cpp - takes readings from Data/Readings/ and merges segments into readings.fasta

---
+++ Code/reading_creator.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+
+    srand(time(0));
+
+    string path = "../Data/Readings";
+    string path_reading = "../Data/reading.fasta";
+
+    try {
+        if (!fs::exists(path_reading)) {
+            cout << "Datoteka ne postoji!" << endl; 
+            ofstream izlaz(path_reading);
+            if (izlaz.is_open()) {
+                izlaz.close();
+            }
+        }
+    } catch (const fs::filesystem_error& e) {
+        cout << "Greška: " << e.what() << endl;
+    }
+    
+    try {
+        
+        ofstream izlaz(path_reading);
+        for (const auto& entry : fs::directory_iterator(path)) {
+            ifstream ocitanje(entry.path());
+            int br_linija = 0;
+            for (string linija; getline(ocitanje, linija); ) {
+                ++br_linija;
+            }
+            ocitanje.close();
+            int br_linija_kroz4 = br_linija/4;
+           
+            int random_number = rand() % (br_linija_kroz4-1);
+            int index = random_number * 4 + 2;
+
+            ocitanje.open(entry.path());
+
+            string linija;
+            int trenutna = 1;
+            while (getline(ocitanje, linija)) {
+                if (trenutna == index) {
+                    break;
+                } else {
+                    trenutna++;
+                }
+            }
+            izlaz << linija << endl;
+        }
+    } catch (const fs::filesystem_error& e) {
+        cout << "Greška: " << e.what() << endl;
+    }
+
+    return 0;
+}
